eaa31d13c78c9df498f6aa1884b0b2d65eabe109
Hakter12 <117771553+Hakter12@users.noreply.github.com>
2023-04-30 12:33:33 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,61 @@
+#pip install flask
+
+from flask import Flask,render_template,url_for,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer,default = 0)
+    pub_date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form('task')
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Здесь произошла ошибка'
+    else:
+        task = Todo.query.order_by(Todo.pub_date).all()
+        return render_template('/index.html',tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Todo.query.get_or_404(id)
+    try:
+        db.seddion.delete(task)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Ошибка при удалении'
+
+@app.route('/update/<int:id>',methods=['POST','GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['Task']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Ошибка при обновлении'
+        else:
+            tasks = Todo.query.order_by(Todo.pub_date).all()
+            return render_template('/index.html',tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
